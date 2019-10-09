14d9752f0de49df376b0f983862de2a0439de34d
marcelmiguel <marcel.miguel@gmail.com>
2019-10-09 21:05:33 +0200

    Add SSD1306 display

--- nodemcu.ino
+++ nodemcu.ino
@@ -2,12 +2,23 @@
 #include <CTBotDataStructures.h>
 #include <CTBotInlineKeyboard.h>
 #include <Utilities.h>
-#include "secrets.h"
+
 #include <Wire.h>
+#include <Adafruit_GFX.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <Adafruit_SSD1306.h>
+#include <splash.h>
 
+#include "secrets.h"
+
+// BME280 constants
 #define SEALEVELPRESSURE_HPA (1013.25)
+
+// SSD1306 constants
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
 #define LED0 3 //D3
 #define LED1 1 //D10
 #define LED2 2 //
@@ -15,12 +26,14 @@
 
 // Global objects
 Adafruit_BME280 bme;
+uint ticks=0;
 float temperature, humidity, pressure, altitude;
 String ssid  = SECRET_SSID;
 String pass  = SECRET_PASS;
 String token = SECRET_BOT_TOKEN;  
 String res = "";
 CTBot myBot;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 
 void setup() {
@@ -40,6 +53,15 @@ void setup() {
   else
     Serial.println("\nNo internet access, check wifi");
     
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  printWeatherInfo();
+
   /*pinMode(LED0, OUTPUT); 
   pinMode(LED1, OUTPUT); 
   pinMode(LED2, OUTPUT); 
@@ -79,6 +101,42 @@ void loop() {
       myBot.sendMessage(msg.sender.id, res);
     }
   }  
-	
-  delay(1000);
+	if (ticks % 10) {
+    printWeatherInfo();
+  }
+
+  ticks ++;
+  delay(5000);
+}
+
+void printWeatherInfo() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  
+  altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  res = altitude;
+  res += "m";
+  display.setCursor(0, 10);
+  display.println(res);
+
+  humidity = bme.readHumidity();
+  res = humidity;
+  res += "%";
+  display.setCursor(0, 20);
+  display.println(res);
+
+  temperature = bme.readTemperature();
+  res = temperature;
+  res += "ºC";
+  display.setCursor(0, 30);
+  display.println(res);
+  
+  pressure = bme.readPressure() / 100.0F;
+  res = pressure;
+  res += "hPa";
+  display.setCursor(0, 40);
+  display.println(res);
+
+  display.display(); 
 }
